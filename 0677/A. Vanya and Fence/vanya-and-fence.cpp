d46c626720710e059c4f6549cd6dac93861f78af
subbus <subbareddyg83@gmail.com>
2022-08-05 23:14:02 +0530

    add 0677 A

--- 0677/A. Vanya and Fence/vanya-and-fence.cpp
+++ 0677/A. Vanya and Fence/vanya-and-fence.cpp
@@ -1,1 +1,70 @@
 // https://codeforces.com/contest/677/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// for cin>>vector
+template <typename T>
+istream &operator>>(istream &in, vector<T> &v)
+{
+
+	T x;
+	char sentinel; // for '['
+	in >> sentinel;
+	while (in >> x)
+	{
+		if (in.peek() == ',') // check if the next character is a comma and ignore it
+			in.ignore();
+		v.push_back(x);
+	}
+	return in;
+}
+
+// for cout<<vector
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &v)
+{
+	out << "[";
+	for_each(begin(v), end(v) - 1, [&](const auto &element)
+			 { out << element << ","; });
+	out << v.back() << "]" << endl;
+	return out;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("0-!n.txt", "r", stdin);
+	freopen("0-out.txt", "w", stdout);
+#endif
+	std::ios::sync_with_stdio(false);
+
+	int t = 1;
+
+#ifndef ONLINE_JUDGE
+	cin >> t;
+#endif
+
+	while (t--)
+	{
+		int n, h;
+		cin >> n >> h;
+		int min_width = 0;
+		while (n--)
+		{
+			int height;
+			cin >> height;
+			if (height <= h)
+			{
+				min_width++;
+			}
+			else
+			{
+				min_width += 2;
+			}
+		}
+		cout << min_width << endl;
+		cout << endl;
+	}
+	return 0;
+}
